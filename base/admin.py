32d2dd213d51e10be81671ca6569cfda519697cd
banpei.lim <banpei.lim@gmail.com>
2022-11-15 11:27:01 +0800

    Initial Commit

---
+++ base/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Flight
+from .models import Review
+from .models import Attendance
+
+# Register your models here.
+
+admin.site.register(Flight)
+admin.site.register(Review)
+admin.site.register(Attendance)
---
+++ base/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('flight-data/', views.getFlights, name="routes"),
+    path('update-review/', views.updateReview, name="update_review"),
+    path('data-page/', views.dataPage.as_view(), name="data_page"),
+    path('update-attendance/', views.updateAttendance, name="update_attendance"),
+
+]
